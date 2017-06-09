eee2050fb02450ce80a0fb78ecbff13077a97f1e
tedwards2412 <tedwards2412@gmail.com>
2017-06-09 16:05:55 +0200

    First attempt at translation without testing at all

--- rockfish/core.py
+++ rockfish/core.py
@@ -16,15 +16,15 @@ class Fish(object):
     def infomatrix(self, solver = "direct"):
         D = opDiagonal(model.noise/model.exposure) + model.systematics
         n = length(model.flux)
-        x = Vector{Vector{Float64}}(n)
+        x = np.zeros(n)
         if solver == "direct":
-            invD = inv(full(D))
+            invD = np.inv(np.full(D))
             for i in range(n):
-                x[i] = invD*model.flux[i]
+                x[i] = np.invD*model.flux[i]
         elif solver == "cg":
             for i in range(n):
-                Pl = opDiagonal(diag(D))
-                x[i] = cg(D, model.flux[i], Pl = Pl, verbose=true, maxiter = 10)
+                Pl = np.diagonal(np.diagonal(D))
+                x[i] = la.cg(D, model.flux[i], Pl = Pl, verbose=True, maxiter=10)
         else:
             error("Solver unknown.")
         I = Matrix{Float64}(n,n)
@@ -40,12 +40,12 @@ class Fish(object):
         n = length(model.flux)
         x = Vector{Vector{Float64}}(n)
         if solver == "direct":
-            invD = inv(full(D))
+            invD = np.inv(full(D))
             for i in range(n):
                 x[i] = invD*model.flux[i]
         elif solver == "cg":
             for i in range(n):
-                x[i] = cg(D, model.flux[i], verbose=true, maxiter = maxiter)
+                x[i] = cg(D, model.flux[i], verbose=True, maxiter = maxiter)
         else:
             raise KeyError("Solver unknown.")
         I = Matrix{Float64}(n,n)
@@ -60,4 +60,62 @@ class Fish(object):
                 I[j,i] = tmp
         return F, I
 
+    def effective(I, i):
+        invI = np.inv(I)
+        return 1/invI[i,i]
 
+    def effective(F, I, i):
+        n = np.size(I, 1)
+        if n == 1:
+            return F[i,i]
+        indices = np.setdiff1d(0:n-1, i)
+        eff_F = F[i,i]
+        C = Vector{Float64}(n-1)
+        B = Matrix{Float64}(n-1,n-1)
+        for j in range(n):
+            C[j] = I[indices[j],i]
+            for k in 1:n-1
+                B[j,k] = I[indices[j], indices[k]]
+        invB = inv(B)
+        for j in range(n):
+            for k in range(n):
+                eff_F = eff_F - F[i,indices[j]]*invB[j,k]*C[k]
+                eff_F = eff_F - C[j]*invB[j,k]*F[indices[k],i]
+                for l in range(n):
+                    for m in range(n):
+                        eff_F = eff_F + C[j]*invB[j,l]*F[indices[l],indices[m]]*invB[m,k]*C[k]
+        return eff_F
+
+    def tensorproduct(Sigma1, Sigma2)
+        return tensorproduct(la.LinearOperator(Sigma1), Sigma2)
+
+    def tensorproduct(Sigma1, Sigma2):
+        return tensorproduct(Sigma1, full(Sigma2))
+
+    def tensorproduct(Sigma1, Sigma2):
+        n1 = size(Sigma1,1)
+        n2 = size(Sigma2,1)
+        N = n1*n2
+        def Sigma(x):
+            A = np.reshape(x, (n1, n2))
+            B = np.zeros(A)
+            for i in 1:n2
+                y = Sigma1*A[:,i]
+                for j in range(n2):
+                    B[:,j] += Sigma2[i,j]*y 
+            return np.reshape(B, N)
+        return la.LinearOperator(N, N, True, True, x->Sigma(x))
+
+    def Sigma_hpx(nside, sigma=0., scale=1.):
+        npix = healpy.nside2npix(nside)
+        def hpxconvolve(x):
+            if sigma != 0.:
+                alm = healpy.map2alm(x.*scale)
+                x = healpy.alm2map(alm, nside, sigma = deg2rad(sigma), verbose=false)
+                return x.*scale
+        def flat(x):
+            return scale*sum(x.*scale)
+        if sigma == np.Inf:
+            return la.LinearOperator(npix, npix, True, True, x->flat(x))
+        else:
+            return la.LinearOperator(npix, npix, True, True, x->hpxconvolve(x))
