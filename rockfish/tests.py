3860bdc8ab0e389d5c203734fc77851e98ef0d54
Christoph Weniger <c.weniger@uva.nl>
2017-07-11 15:26:50 +0200

    Updates

--- rockfish/tests.py
+++ rockfish/tests.py
@@ -4,100 +4,12 @@
 from __future__ import division
 import healpy as hp
 import numpy as np
-import scipy.sparse.linalg as la
 import HARPix as harp
 import pylab as plt
 from core import *
-
-class HARPix_Sigma(la.LinearOperator):
-    """docstring for CovarianceMatrix"""
-    def __init__(self, harpix):
-        self.harpix = harpix
-        self.N = np.prod(np.shape(self.harpix.data))
-        super(HARPix_Sigma, self).__init__(None, (self.N, self.N))
-        self.Flist = []
-        self.Glist = []
-        self.Slist = []
-        self.Tlist = []
-        self.nsidelist = []
-
-    def add_systematics(self, err = None, sigmas = None, Sigma = None,
-            nside = None):
-        F = err.get_formatted_like(self.harpix).get_data(mul_sr=True)
-        self.Flist.append(F)
-
-        lmax = 3*nside - 1  # default from hp.smoothing
-        Nalm = hp.Alm.getsize(lmax)
-        G = np.zeros(Nalm, dtype = 'complex128')
-        for sigma in sigmas:
-            H = hp.smoothalm(np.ones(Nalm), sigma = np.deg2rad(sigma), inplace = False, verbose = False)
-            npix = hp.nside2npix(nside)
-            m = np.zeros(npix)
-            m[10] = 1
-            M = hp.alm2map(hp.map2alm(m)*H, nside, verbose = False)
-            G += H/max(M)
-        G /= len(sigmas)
-        self.Glist.append(G)
-        T = harp.get_trans_matrix(self.harpix, nside, nest = False, counts = True)
-        self.Tlist.append(T)
-        self.nsidelist.append(nside)
-
-        self.Slist.append(Sigma)
-
-    def _matvec(self,x):
-        result = np.zeros(self.N)
-        for nside, F, G, S, T in zip(self.nsidelist, self.Flist, self.Glist, self.Slist, self.Tlist):
-            y = x.reshape((-1,)+self.harpix.dims)*F
-            z = harp.trans_data(T, y)
-            if S is not None:
-                a = S.dot(z.T).T
-            else:
-                a = z
-            b = np.zeros_like(z)
-            if self.harpix.dims is not ():
-                for i in range(self.harpix.dims[0]):
-                    alm = hp.map2alm(a[:,i])
-                    alm *= G
-                    b[:,i] = hp.alm2map(alm, nside, verbose = False)
-            else:
-                alm = hp.map2alm(a, iter = 0)  # Older but faster routine
-                alm *= G
-                b = hp.alm2map(alm, nside, verbose = False)
-            c = harp.trans_data(T.T, b)
-            d = c.reshape((-1,)+self.harpix.dims)*F
-            result += d.flatten()
-        return result
-
-def get_sigma(x, f):
-    X, Y = np.meshgrid(x,x)
-    Sigma = f(X,Y)
-    A = 1/np.sqrt(np.diag(Sigma))
-    Sigma = np.diag(A).dot(Sigma).dot(np.diag(A))
-    return Sigma
-
-def get_model_input(signals, noise, systematics, exposure):
-    # Everything is intensity
-    S = [sig.get_formatted_like(signals[0]).get_data(mul_sr=True).flatten() for sig in signals]
-    N = noise.get_formatted_like(signals[0]).get_data(mul_sr=True).flatten()
-    SYS = HARPix_Sigma(signals[0])
-    if systematics is None:
-        SYS = None
-    else:
-        for sys in systematics:
-            SYS.add_systematics(**sys)
-    if isinstance(exposure, float):
-        E = np.ones_like(N)*exposure
-    else:
-        E = exposure.get_formatted_like(signals[0]).get_data().flatten()
-    return S, N, SYS, E
-
-
+from tools import *
 
 def test_3d():
-    from HARPix import HARPix
-    import healpy as hp
-    import pylab as plt
-
     def plot_harp(h, filename, dims = ()):
         m = h.get_healpix(128, idxs= dims)
         hp.mollview(m, nest=True)
@@ -127,7 +39,7 @@ def test_3d():
 
     # Signal definition
     spec_sig = np.exp(-(x-5)**2/2)
-    sig = HARPix(dims = dims).add_iso(nside).add_singularity( (50,50), 1, 20, n = 10)
+    sig = harp.HARPix(dims = dims).add_iso(nside).add_singularity( (50,50), 1, 20, n = 10)
     sig.add_func(lambda d: spec_sig*np.exp(-d**2/2/20**2), mode = 'dist', center=(0,0))
     sig.add_func(lambda d: spec_sig/(d+1)**1, mode = 'dist', center=(50,50))
     sig.mul_sr()
@@ -159,12 +71,8 @@ def test_3d():
 
 
 def test_UL():
-    from HARPix import HARPix
-    import healpy as hp
-    import pylab as plt
-
     # Signal definition
-    sig = HARPix().add_iso(16)
+    sig = harp.HARPix().add_iso(16)
     sig.add_func(lambda d: np.exp(-d**2/2/20**2), mode = 'dist', center=(0,0))
 
     # Background definition
@@ -191,10 +99,6 @@ def test_UL():
     print "Upper limit on theta (gaussian):", ULg
 
 def test_simple():
-    from HARPix import HARPix
-    import healpy as hp
-    import pylab as plt
-
     def plot_harp(h, filename):
         m = h.get_healpix(128)
         hp.mollview(m, nest=True)
@@ -205,7 +109,7 @@ def test_simple():
 
     # Signal definition
     spec = 1.
-    sig = HARPix(dims = dims).add_iso(nside).add_singularity( (50,50), .1, 20, n = 100)
+    sig = harp.HARPix(dims = dims).add_iso(nside).add_singularity( (50,50), .1, 20, n = 100)
     sig.add_func(lambda d: np.exp(-d**2/2/20**2), mode = 'dist', center=(0,0))
     sig.add_func(lambda d: 1/(d+1)**1, mode = 'dist', center=(50,50))
     sig.data += 0.1  # EGBG
@@ -238,10 +142,6 @@ def test_simple():
     quit()
 
 def test_MW_dSph():
-    from HARPix import HARPix
-    import healpy as hp
-    import pylab as plt
-
     def plot_harp(h, filename):
         m = h.get_healpix(128)
         hp.mollview(np.log10(m), nest=True)
@@ -251,17 +151,17 @@ def test_MW_dSph():
 
     # Signal definition
     spec = 1.
-    MW = HARPix(dims = dims).add_iso(8).add_singularity((0,0), 0.1, 20, n = 100)
+    MW = harp.HARPix(dims = dims).add_iso(8).add_singularity((0,0), 0.1, 20, n = 100)
     MW.add_func(lambda d: spec/(.1+d)**2, mode = 'dist', center=(0,0))
     pos = (50, 40)
-    dSph = HARPix(dims = dims).add_singularity(pos, 0.1, 20, n = 100)
+    dSph = harp.HARPix(dims = dims).add_singularity(pos, 0.1, 20, n = 100)
     dSph.add_func(lambda d: 0.1*spec/(.1+d)**2, mode = 'dist', center=pos)
     sig = MW + dSph
     sig.data += 1  # EGBG
     plot_harp(sig, 'sig.eps')
 
     # Background definition
-    bg = HARPix(dims = dims).add_iso(64)
+    bg = harp.HARPix(dims = dims).add_iso(64)
     bg.add_func(lambda l, b: 1/(b+1)**2)
     plot_harp(bg, 'bg.eps')
 
@@ -284,8 +184,6 @@ def test_MW_dSph():
     plot_harp(f, 'test.eps')
 
 def test_spectra():
-    import pylab as plt
-
     x = np.linspace(0, 10, 1000)  # energy
     dx = x[1]-x[0]  # bin size
 
@@ -325,14 +223,10 @@ def smoothtest():
     plt.savefig('test.eps')
 
 def test_covariance():
-    from HARPix import HARPix
-    import healpy as hp
-    import pylab as plt
-
     nside = 8
 
     # Signal definition
-    sig = HARPix().add_iso(nside).add_singularity( (50,50), .1, 50, n = 100)
+    sig = harp.HARPix().add_iso(nside).add_singularity( (50,50), .1, 50, n = 100)
     sig.add_func(lambda d: np.exp(-d**2/2/20**2), mode = 'dist', center=(0,0))
     sig.mul_sr()
 
@@ -353,8 +247,6 @@ def test_covariance():
     plt.savefig('test.eps')
 
 def test_matrix():
-    import pylab as plt
-
     x = np.linspace(0, 10, 1000)  # energy
     dx = x[1]-x[0]  # bin size
 
@@ -380,10 +272,10 @@ def test_matrix():
 
 
 if __name__ == "__main__":
-    test_3d()
+    #test_3d()
     #test_covariance()
     #test_simple()
     #test_UL()
     #smoothtest()
     #test_spectra()
-    #test_matrix()
+    test_matrix()
