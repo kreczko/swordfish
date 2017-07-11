bb75307d80d818fd6863e810d29e8e90a1a80da2
Christoph Weniger <c.weniger@uva.nl>
2017-07-11 12:25:07 +0200

    Updates

--- rockfish/core.py
+++ rockfish/core.py
@@ -8,12 +8,14 @@ import scipy.sparse.linalg as la
 import scipy.sparse as sp
 import HARPix as harp
 import pylab as plt
+from copy import deepcopy
 
 class Model(object):
     def __init__(self, flux, noise, systematics, exposure, solver = 'direct'):
         self.flux = flux
         self.noise = noise
         self.exposure = exposure
+        self.cache = None
 
         self.solver = solver
         self.nbins = len(self.noise)  # Number of bins
@@ -25,14 +27,13 @@ class Model(object):
                     (self.nbins, self.nbins), matvec = lambda x: x*0.)
 
     def solveD(self, thetas = None, psi = 1.):
-        # FIXME: Cache results
-        noise = self.noise
+        noise = deepcopy(self.noise)
         exposure = self.exposure*psi
         if thetas is not None: 
             for i in range(max(self.ncomp, len(thetas))):
                 noise += thetas[i]*self.flux[i]
         D = (
-                la.aslinearoperator(sp.diags(self.noise/self.exposure))
+                la.aslinearoperator(sp.diags(noise/exposure))
                 + self.systematics
                 )
         x = np.zeros((self.ncomp, self.nbins))
@@ -43,9 +44,11 @@ class Model(object):
                 x[i] = np.dot(invD, self.flux[i])
         elif self.solver == "cg":
             def callback(x):
-                print len(x), sum(x), np.mean(x)
+                pass
+                #print len(x), sum(x), np.mean(x)
             for i in range(self.ncomp):
-                x[i] = la.cg(D, self.flux[i], callback = callback, tol = 1e-5)[0]
+                x[i] = la.cg(D, self.flux[i], x0 = self.cache, callback = callback, tol = 1e-5)[0]
+                self.cache= x[i]
         else:
             raise KeyError("Solver unknown.")
         return x, noise, exposure
@@ -104,16 +107,21 @@ class EffectiveCounts(object):
     def __init__(self, model):
         self.model = model
 
+    def counts(self, i, theta):
+        return sum(self.model.flux[i]*self.model.exposure*theta)
+
     def effectivecounts(self, i, theta, psi = 1.):
         I0 = self.model.effectivefishermatrix(i, psi = psi)
         thetas = np.zeros(self.model.ncomp)
         thetas[i] = theta
         I = self.model.effectivefishermatrix(i, thetas = thetas, psi = psi)
+        if I0 == I:
+            return 0., None
         s = 1/(1/I-1/I0)*theta**2
         b = 1/I0/(1/I-1/I0)**2*theta**2
         return s, b
 
-    def upperlimit(self, alpha, i, psi = 1., gaussian = True):
+    def upperlimit(self, alpha, i, psi = 1., gaussian = False):
         Z = 2.64  # FIXME
         I0 = self.model.effectivefishermatrix(i, psi = psi)
         if gaussian:
@@ -123,15 +131,19 @@ class EffectiveCounts(object):
             thetaUL_est = Z/np.sqrt(I0)  # Gaussian estimate
             thetas[i] = thetaUL_est
             I = self.model.effectivefishermatrix(i, thetas = thetas, psi = psi)
-            if (I0-I)<0.01*I:
+            if (I0-I)<0.02*I:  # 1% accuracy of limits
                 thetaUL = thetaUL_est
             else:
-                raise NotImplementedError()  # FIXME Finish implementation
-                theta_list = np.linspace(thetaUL_est/1000, thetaUL_est*1000, 1000)
                 z_list = []
-                for theta in theta_list:
+                theta_list = [thetaUL_est/1]
+                while True:
+                    theta = theta_list[-1]
                     s, b = self.effectivecounts(i, theta = theta, psi = psi)
                     z_list.append(s/np.sqrt(s+b))
+                    if z_list[-1] > Z:
+                        break
+                    else:
+                        theta_list.append(theta*1.5)
                 thetaUL = np.interp(Z, z_list, theta_list)
             return thetaUL
 
@@ -191,7 +203,7 @@ class HARPix_Sigma(la.LinearOperator):
             npix = hp.nside2npix(nside)
             m = np.zeros(npix)
             m[10] = 1
-            M = hp.alm2map(hp.map2alm(m)*H, nside)
+            M = hp.alm2map(hp.map2alm(m)*H, nside, verbose = False)
             G += H/max(M)
         G /= len(sigmas)
         self.Glist.append(G)
@@ -319,6 +331,56 @@ def test_3d():
     #plot_rock(f, 'test.eps')
     quit()
 
+def test_UL():
+    from HARPix import HARPix
+    import healpy as hp
+    import pylab as plt
+
+    dims = ()
+    nside = 16
+
+    # Signal definition
+    spec = 1.
+    sig = HARPix(dims = dims).add_iso(nside)#.add_singularity( (50,50), .1, 20, n = 100)
+    sig.add_func(lambda d: np.exp(-d**2/2/20**2), mode = 'dist', center=(0,0))
+    #sig.add_func(lambda d: 1/(d+1)**1, mode = 'dist', center=(50,50))
+    #sig.data += 0.1  # EGBG
+    sig.mul_sr()
+    #sig.print_info()
+
+    # Background definition
+    bg = harp.zeros_like(sig)
+    bg.add_func(lambda l, b: 0./(b**2+1.)**0.5+1.0)
+    bg.mul_sr()
+    #bg.print_info()
+
+    # Covariance matrix definition
+
+    cov = HARPix_Sigma(sig)
+    var = bg*bg
+    var.data *= 0.  # 10% uncertainty
+    var.data += 0.01  # 10% uncertainty
+    var.mul_sr()
+    var.mul_sr()
+    cov.add_systematics(variance = var, sigmas = [20.,], Sigma = None, nside = 64)
+
+    # Set up rockfish
+    fluxes = [sig.data.flatten()]
+    noise = bg.data.flatten()
+    systematics = cov
+    exposure = np.ones_like(noise)*100000.0
+    m = Model(fluxes, noise, systematics, exposure, solver='cg')
+
+    ec = EffectiveCounts(m)
+
+    UL = ec.upperlimit(0.05, 0)
+    ULg = ec.upperlimit(0.05, 0, gaussian = True)
+
+    print "Total signal counts (theta = 1):", ec.counts(0, 1.0)
+    print "Eff.  signal counts (theta = 1):", ec.effectivecounts(0, 1.0)[0]
+    print "Upper limit on theta:           ", UL
+    print "Upper limit on theta (gaussian):", ULg
+
 def test_simple():
     from HARPix import HARPix
     import healpy as hp
@@ -363,13 +425,13 @@ def test_simple():
     fluxes = [sig.data.flatten()]
     noise = bg.data.flatten()
     systematics = cov
-    exposure = np.ones_like(noise)*0.1
+    exposure = np.ones_like(noise)*100.
     m = Model(fluxes, noise, systematics, exposure, solver='cg')
 
 #    I = m.fishermatrix()
 #    F = m.infoflux()
 #    I = m.effectivefishermatrix(0)
-    F = m.effectiveinfoflux(0)
+    F = m.effectiveinfoflux(0, thetas = [0.000], psi = 1.)
     f = harp.HARPix.from_data(sig, F)
     f.div_sr()
     plot_harp(f, 'test.eps')
@@ -507,9 +569,39 @@ def test_covariance():
     hp.mollview(z, nest = True)
     plt.savefig('test.eps')
 
+def test_matrix():
+    import pylab as plt
+
+    x = np.linspace(0, 10, 1000)  # energy
+    dx = x[1]-x[0]  # bin size
+
+    fluxes = [np.exp(-(x-5)**2/2/3**2)*dx,np.exp(-(x-5)**2/2/0.2**2)*dx]
+    noise = np.ones_like(x)*dx
+    exposure = np.ones_like(noise)*1.001
+    X, Y = np.meshgrid(x,x)
+    systematics = 0.1*(
+            np.diag(noise).dot(
+                np.exp(-(X-Y)**2/2/40**2) + np.exp(-(X-Y)**2/2/20**2)
+                #np.exp(-(X-Y)**2/2/10**2) + np.exp(-(X-Y)**2/2/5**2) +
+                #np.exp(-(X-Y)**2/2/2**2) + np.exp(-(X-Y)**2/2/1**2)
+                )).dot(np.diag(noise))
+    m = Model(fluxes, noise, systematics, exposure, solver='cg')
+    I = m.fishermatrix()
+    print I
+    F = m.infoflux()
+    f = m.effectiveinfoflux(0)
+    plt.plot(x, f*dx, label='Feff')
+    plt.plot(x, F[0,0]*dx, label='F00')
+    plt.plot(x, F[1,1]*dx, label='F11')
+    plt.plot(x, F[0,1]*dx, label='F01')
+    plt.legend()
+    plt.savefig('test.eps')
+
 if __name__ == "__main__":
     #test_3d()
     #test_covariance()
-    test_simple()
+    #test_simple()
+    #test_UL()
     #smoothtest()
     #test_spectra()
+    test_matrix()
