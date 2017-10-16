77e0e92cfaf618a9816a71b041ae1bbd5763c34a
Christoph Weniger <c.weniger@uva.nl>
2017-10-16 22:01:47 +0200

    Update setup files

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from distutils.core import setup
 
 setup(
     name='swordfish',
-    version='0.1',
-    description='Your sword for Fisher forecasting.',
+    version='0.2.0',
+    description='Predicting the Information Yield of Counting Experiments.',
     author='Thomas Edwards and Christoph Weniger',
     author_email='c.weniger@uva.nl',
     packages=['swordfish'],
     package_data={'swordfish': [] },
-    long_description="""swordfish is a Python tool to study the information gain of counting experiments.""",
+    long_description=open('README.md').read(),
 )
